987114c92165b77cb314afcea92fe6e1aebbbf6e
btroxel-matc <112974528+btroxel-matc@users.noreply.github.com>
2023-10-17 14:12:06 -0500

    sns.py

--- sns.py
+++ sns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+#Author: Bryan Troxel
+#Description: This simple script creates an sns topic to be sent out as an alert through email. It is meant to be used within another script in which it can be told where to send the email.
 import boto3
 
 def CreateSNSTopic(topicName):
